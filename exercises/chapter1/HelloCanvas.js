809c593c89d15b4bb6ce939dc2ef12e97cb90072
Daniel Nwankwo <dan_nwankwo@yahoo.co.uk>
2017-08-21 17:42:15 +0100

    Commit example files

--- exercises/chapter1/HelloCanvas.js
+++ exercises/chapter1/HelloCanvas.js
@@ -5,6 +5,6 @@ function main(){
         console.log("No cntext available");
         return;
     }
-    gl.clearColor(0.0,0.0,0.0,1.0);
+    gl.clearColor(1.0,0.5,0.5,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
